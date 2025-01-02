3f3bc128a3267634d5538dbc5f293aa2ca722ae0
JFVela <jfvela_2012@hotmail.com>
2025-01-02 15:44:39 -0500

    terminado

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -2,9 +2,18 @@ import Banner from "../../components/Banner";
 import Titulo from "../../components/Titulo";
 import styles from "./index.module.css";
 import Card from "../../components/Card";
-import Videos from "../../data/db.json";
+import { useEffect, useState } from "react";
 
 function Inicio() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/JFVela/api-cinema-alura/videos")
+      .then((response) => response.json())
+      .then((data) => setVideos(data))
+      .catch((error) => console.error("Error fetching videos:", error)); // Agregado para manejar errores.
+  }, []);
+
   return (
     <>
       <Banner img="home" color="#154580" />
@@ -12,7 +21,7 @@ function Inicio() {
         <h1>Un Lugar Para guardar tus videos favoritos</h1>
       </Titulo>
       <section className={styles.container}>
-        {Videos.map((video) => {
+        {videos.map((video) => {
           return <Card {...video} key={video.id} />;
         })}
       </section>
--- src/pages/Player/index.jsx
+++ src/pages/Player/index.jsx
@@ -1,13 +1,24 @@
 import styles from "./Player.module.css";
 import Titulo from "../../components/Titulo";
-import videos from "../../data/db.json";
+//import videos from "../../data/db.json";
 import Banner from "../../components/Banner";
 import { useParams } from "react-router-dom";
 import NotFound from "../NotFound";
-
+import { useEffect, useState } from "react";
 function Player() {
-  const parametros = useParams();
-  const video = videos.find((video) => video.id === Number(parametros.id));
+  const [video, setVideo] = useState([]);  const parametros = useParams();
+  useEffect(() => {
+    fetch(
+      `https://my-json-server.typicode.com/JFVela/api-cinema-alura/videos?id=${parametros.id}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setVideo(...data);
+      });
+  }, []);
+
+  //const parametros = useParams();
+  //const video = videos.find((video) => video.id === Number(parametros.id));
 
   if (!video) return <NotFound />;
 
@@ -15,7 +26,7 @@ function Player() {
     <>
       <Banner img="player" color="#58B9AE" />
       <Titulo>
-        <h1>Player</h1>
+        <h1>{video.titulo}</h1>
       </Titulo>
       <section className={styles.contenido}>
         <div className={styles.videoWrapper}>
